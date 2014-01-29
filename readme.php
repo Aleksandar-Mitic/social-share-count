beace2d16545c450dd03566e4cbfc3124cc3f348
webhkp <webhkp@gmail.com>
2014-01-29 21:44:53 +0600

    readme

---
+++ readme.php
@@ -0,0 +1,8 @@
+Social Share Count
+==================
+
+This class gets share count from sites. Currently it get share count from facebook, twitter, google plus, linkedin, pinterest, delicious
+
+Implementation
+--------------
+Implementation is very easy of the class. Example of implementation is provide in implement.php
